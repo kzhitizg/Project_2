e5353dd9341ad14c268d75c9fb3e12ee8c6eb168
Kshitiz Gehlot <kshitizg99@gmail.com>
2021-01-31 20:07:38 +0530

    Converted neighbour region growing to function

--- Region_Growing/neighbour_region_growing.py
+++ Region_Growing/neighbour_region_growing.py
@@ -16,18 +16,6 @@ import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
 from progress.bar import IncrementalBar
 
-t = time.time()
-
-# load an image
-img = cv2.imread("input3.jpg")
-
-thr = 20
-
-nbrs = np.array([
-    [0, -1],
-    [-1, 0]
-    ], "int8")
-
 # -------------------- Classes -----------------------
 
 class Stack:
@@ -55,13 +43,6 @@ class Stack:
     def __len__(self):
         return self.i
 
-# Generator to give neighbours
-def get_neighbour(i :int, j:int):
-    global img, nbrs
-
-    for n in nbrs:
-        if i+n[0] >= 0 and j+n[1]>=0:
-            yield i+n[0], j+n[1], img[i+n[0],j+n[1]]
 
 def iterate_image(img, delta_r_start = 0, delta_r_end = 0, delta_c_start = 0, delta_c_end = 0):
     for i in range(delta_r_start, img.shape[0]+delta_r_end):
@@ -86,63 +67,94 @@ def eucl_dist(v1, v2):
 
 # ----------------------------------- Main ----------------------------------
 
-regions = np.zeros(img.shape[:-1], "uint64")
-n_reg = 0
+def segment(path, thr):
+    t = time.time()
 
-mapping = {}
+    # load an image
+    img = cv2.imread(path)
+    
+    # Generator to give neighbours
+    def get_neighbour(i :int, j:int):
+        nbrs = np.array([
+            [0, -1],
+            # [0, -2],
+            # [-2, 0],
+            [-1, 0]
+            ], "int8")
+        for n in nbrs:
+            if i+n[0] >= 0 and j+n[1]>=0:
+                yield i+n[0], j+n[1]
 
-bar = IncrementalBar('Region Init', max=img.shape[0]*img.shape[1])
+    regions = np.zeros(img.shape[:-1], "uint64")
+    n_reg = 0
 
-for i1, j1, val in iterate_image(img):
-    flag = 0
-    for i2, j2, _ in get_neighbour(i1, j1):
-        if eucl_dist(val, img[i2, j2]) < thr:
-            flag = 1
-            if regions[(i1, j1)] == 0:
-                regions[(i1, j1)] = regions[(i2, j2)]
-            else:
-                if regions[(i1, j1)] != regions[(i2, j2)]:
-                    frm = regions[(i1, j1)]
-                    to = regions[(i2, j2)]
-                    if frm != to:
-                        mapping[max(frm, to)] = min(frm, to)
+    mapping = {}
 
-    if flag == 0:
-        n_reg +=1
-        regions[(i1, j1)] = n_reg
-    bar.next()
+    bar = IncrementalBar('Region Init', max=img.shape[0]*img.shape[1])
+
+    for i1, j1, val in iterate_image(img):
+        flag = 0
+        for i2, j2 in get_neighbour(i1, j1):
+            if eucl_dist(val, img[i2, j2]) < thr:
+                flag = 1
+                if regions[(i1, j1)] == 0:
+                    regions[(i1, j1)] = regions[(i2, j2)]
+                else:
+                    if regions[(i1, j1)] != regions[(i2, j2)]:
+                        frm = regions[(i1, j1)]
+                        to = regions[(i2, j2)]
+                        while frm in mapping.keys():
+                            frm = mapping[frm]
+                        while to in mapping.keys():
+                            to = mapping[to]
+                        if frm != to:
+                            mapping[max(frm, to)] = min(frm, to)
+
+        if flag == 0:
+            n_reg +=1
+            regions[(i1, j1)] = n_reg
+        bar.next()
 
 
-# plt.matshow(regions)
-# plt.show()
+    # plt.matshow(regions)
+    # plt.show()
 
-col_arr = []
-colors = set()
+    col_arr = []
+    colors = set()
 
-col_map = []
+    col_map = []
 
-for c in range(1, n_reg+1):
-    if c not in mapping.keys():
-        col_map.append(gen_color(colors))
-    else:
-        val = c
-        while val in mapping.keys():
-            # print(val)
-            val = mapping[val]
-        # print(c, mapping[c], val, len(col_map))
-        col_map.append(col_map[int(val-1)])
+    print()
+    bar = IncrementalBar('Region Combine', max=n_reg)
 
-output = np.empty_like(img)
+    for c in range(1, n_reg+1):
+        if c not in mapping.keys():
+            col_map.append(gen_color(colors))
+        else:
+            val = c
+            while val in mapping.keys():
+                # print(val)
+                val = mapping[val]
+            # print(c, mapping[c], val, len(col_map))
+            col_map.append(col_map[int(val-1)])
+        bar.next()
+    output = np.empty_like(img)
 
-for i, j, val in iterate_image(regions):
-    output[i,j, :] = col_map[int(val-1)]
+    for i, j, val in iterate_image(regions):
+        output[i,j, :] = col_map[int(val-1)]
 
-# plt.imshow(output)
-# plt.show()
+    # plt.imshow(output)
+    # plt.show()
 
-cv2.imwrite("Output_nbr.jpg", output)
+    cv2.imwrite("10Output_nbr"+path, output)
 
-# cv2.waitKey(0)
-print()
-print(time.time() - t)
-print(n_reg)
+    # # cv2.waitKey(0)
+    print()
+    print(time.time() - t)
+    print(n_reg)
+
+
+if __name__ == "__main__":
+    segment("1.jpg", 10)
+    segment("2.jpg", 10)
+    segment("3.jpg", 10)
