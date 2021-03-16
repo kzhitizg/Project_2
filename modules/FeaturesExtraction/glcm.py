0345b1f073640202701f186ad6fc5f4b2c2b64e5
Kshitiz Gehlot <kshitizg99@gmail.com>
2021-03-16 13:00:37 +0530

    ReDefined private functions in glcm

--- modules/FeaturesExtraction/glcm.py
+++ modules/FeaturesExtraction/glcm.py
@@ -8,20 +8,20 @@ def WithoutBgNorm(mat):
 
     return mat/np.sum(mat)
 
-def glcmMean(mat):
+def _glcmMean(mat):
     return (np.sum(np.arange(0, 256)*np.sum(mat, axis = 1)), 
             np.sum(np.arange(0, 256)*np.sum(mat, axis=0)))
 
-def glcmSd(mat, mux, muy):
+def _glcmSd(mat, mux, muy):
     return np.sqrt([np.sum(np.square(np.arange(0, 256)-mux)*np.sum(mat, axis = 1)), 
             np.sum(np.square(np.arange(0, 256)-muy)*np.sum(mat, axis=0))])
 
-def zeroSafeLog(mat):
+def _zeroSafeLog(mat):
     ret = np.zeros_like(mat)
     ret[mat != 0] = np.log(mat[mat != 0])
     return ret
 
-def probSumDist(mat):
+def _probSumDist(mat):
     ret= np.zeros(256*2 -1)
 
     for i in range(256):
@@ -30,7 +30,7 @@ def probSumDist(mat):
 
     return ret
 
-def probDiffDist(mat):
+def _probDiffDist(mat):
     ret = np.zeros(256)
 
     for i in range(256):
@@ -39,35 +39,35 @@ def probDiffDist(mat):
 
     return ret
 
-def diffAverage(mat, pdd = None):
+def _diffAverage(mat, pdd = None):
     if pdd is None:
-        pdd = probDiffDist(mat)
+        pdd = _probDiffDist(mat)
     return np.sum(np.arange(0, 256)*pdd)
 
-def px(mat):
+def _px(mat):
     return np.sum(mat, axis = 1)
 
-def py(mat):
+def _py(mat):
     return np.sum(mat, axis = 0)
 
-def hxy(mat):
+def _hxy(mat):
     return Entropy(mat)
 
-def hx(mat):
-    ret = px(mat)*zeroSafeLog(px(mat))
+def _hx(mat):
+    ret = _px(mat)*_zeroSafeLog(_px(mat))
     return -np.sum(ret)
 
-def hy(mat):
-    ret = py(mat)*zeroSafeLog(py(mat))
+def _hy(mat):
+    ret = _py(mat)*_zeroSafeLog(_py(mat))
     return -np.sum(ret)
 
-def hxy1(mat):
-    ret = mat*zeroSafeLog(px(mat).reshape(256, 1)*py(mat).reshape(1, 256))
+def _hxy1(mat):
+    ret = mat*_zeroSafeLog(_px(mat).reshape(256, 1)*_py(mat).reshape(1, 256))
     return -np.sum(ret)
 
-def hxy2(mat):
-    pxy = px(mat).reshape(256, 1)*py(mat).reshape(1, 256)
-    ret = pxy*zeroSafeLog(pxy)
+def _hxy2(mat):
+    _pxy = _px(mat).reshape(256, 1)*_py(mat).reshape(1, 256)
+    ret = _pxy*_zeroSafeLog(_pxy)
     return -np.sum(ret)
 
 # ------------ TO BE EXPORTED ----------------
@@ -75,7 +75,7 @@ def ASM(mat):
     return np.sum(np.square(mat))
 
 def Entropy(mat):
-    ret = mat*zeroSafeLog(mat)
+    ret = mat*_zeroSafeLog(mat)
     return -np.sum(ret)
 
 def Contrast(mat):
@@ -89,52 +89,52 @@ def InverseDiffMoment(mat):
     return np.sum((1/(1+np.square(i-j)))*mat)
 
 def Correlation(mat):
-    mux, muy = glcmMean(mat)
-    sdx, sdy = glcmSd(mat, mux, muy)
+    mux, muy = _glcmMean(mat)
+    sdx, sdy = _glcmSd(mat, mux, muy)
     j = np.arange(0, 256)*np.ones((256, 1))
     i = j.T
     return np.sum(mat*((i-mux)*(j-muy)/(sdx*sdy)))
 
 def Variance(mat):
-    sd = glcmSd(mat, *glcmMean(mat))
+    sd = _glcmSd(mat, *_glcmMean(mat))
     return sd[0]*sd[1]
 
 def SumAverage(mat, psd = None):
     if psd is None:
-        psd = probSumDist(mat)
+        psd = _probSumDist(mat)
     return np.sum(np.arange(0, 256*2-1)*psd)
 
 def SumEntropy(mat, psd = None):
     if psd is None:
-        psd = probSumDist(mat)
-    ret = zeroSafeLog(psd)*psd
+        psd = _probSumDist(mat)
+    ret = _zeroSafeLog(psd)*psd
     return -np.sum(ret)
 
 def SumVariance(mat, psd=None, sumavg = None):
     if psd is None:
-        psd = probSumDist(mat)
+        psd = _probSumDist(mat)
     if sumavg is None:
         sumavg = SumAverage(mat, psd)
     return np.sum(np.square(np.arange(0, 256*2-1)-sumavg)*psd)
 
 def DiffVariance(mat, pdd=None, diffavg = None):
     if pdd is None:
-        pdd = probDiffDist(mat)
+        pdd = _probDiffDist(mat)
     if diffavg is None:
-        diffavg = diffAverage(mat, pdd)
+        diffavg = _diffAverage(mat, pdd)
     return np.sum(np.square(np.arange(0, 256)-diffavg)*pdd)
 
 def DiffEntropy(mat, pdd=None):
     if pdd is None:
-        pdd = probDiffDist(mat)
-    ret = zeroSafeLog(pdd)*pdd
+        pdd = _probDiffDist(mat)
+    ret = _zeroSafeLog(pdd)*pdd
     return -np.sum(ret)
 
 def InverseMeasureOfCorrelation1(mat):
-    return (hxy(mat) - hxy1(mat))/max(hx(mat), hy(mat))
+    return (_hxy(mat) - _hxy1(mat))/max(_hx(mat), _hy(mat))
 
 def InverseMeasureOfCorrelation2(mat):
-    return np.sqrt(1-np.exp(-2*(hxy2(mat)-hxy(mat))))
+    return np.sqrt(1-np.exp(-2*(_hxy2(mat)-_hxy(mat))))
 
 def AllFeatures(mat):
     """Used to calculate 13 features of a single GLCM matrix
@@ -152,13 +152,13 @@ def AllFeatures(mat):
     res[3] = Entropy(mat)
     res[4] = Correlation(mat)
     res[5] = Variance(mat)
-    psd = probSumDist(mat)
+    psd = _probSumDist(mat)
     res[6] = SumAverage(mat, psd)
     res[7] = SumEntropy(mat, psd)
     res[8] = SumVariance(mat, psd, res[6])
-    pdd = probDiffDist(mat)
+    pdd = _probDiffDist(mat)
     res[9] = DiffEntropy(mat, pdd)
-    res[10] = DiffVariance(mat, pdd, diffAverage(mat))
+    res[10] = DiffVariance(mat, pdd, _diffAverage(mat))
     res[11] = InverseMeasureOfCorrelation1(mat)
     res[12] = InverseMeasureOfCorrelation2(mat)
 
